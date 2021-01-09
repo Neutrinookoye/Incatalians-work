fe18ee5329884ed58eb2933f245fe299513d7d56
Neutrinookoye <56587885+Neutrinookoye@users.noreply.github.com>
2021-01-09 22:28:29 +0100

    done

--- javascript/index.js
+++ javascript/index.js
@@ -10,7 +10,7 @@
     loop: true,
     autoplayHoverPause: false,
     autoplay: true,
-    smartSpeed: 3000,
+    smartSpeed: 3000, 
     responsiveClass: true,
     responsive: {
       0: {
@@ -22,6 +22,76 @@
     }
   });
 
+
+  // Stats carousel (uses the Owl Carousel library)
+  $('.owl-stats').owlCarousel({
+    animateOut: 'fadeOutLeft',
+    dots : false ,
+    loop: true,
+    autoplayHoverPause: false,
+    autoplay: true,
+    smartSpeed: 6000, 
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      992 : {
+        items : 1,
+      },
+      1000: {
+        items: 2,
+      }
+    }
+  });
+
+  // jQuery counterUp
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000
+  });
+
+  /*--/ Navbar Menu Reduce /--*/
+	$(window).trigger('scroll');
+	$(window).on('scroll', function () { 
+		var pixels = 50; 
+		var top = 1200;
+		if ($(window).scrollTop() > pixels) {
+			$('.navbar-expand-lg').addClass('navbar-reduce');
+			$('.navbar-expand-lg').removeClass('navbar-trans');
+		} else {
+			$('.navbar-expand-lg').addClass('navbar-trans');
+			$('.navbar-expand-lg').removeClass('navbar-reduce');
+		}
+		if ($(window).scrollTop() > top) {
+			$('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
+		} else {
+			$('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
+		}
+	});
+
+  $('.navbar-toggler').on('click', function() {
+    if( ! $('#mainNav').hasClass('navbar-reduce')) {
+      $('#mainNav').addClass('navbar-reduce');
+    }
+  })
+
+
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+  
+  
 
 })(jQuery);
 new WOW().init();
